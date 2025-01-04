42e7e9f6d8aa3824a9be564a76a93ba8956b3167
Dimitri Tkavadze <dtkav22@freeuni.edu.ge>
2025-01-04 14:09:49 +0400

    added recursion strategy

--- src/main/java/com/example/demo/model/RecursionStrategy.java
+++ src/main/java/com/example/demo/model/RecursionStrategy.java
@@ -3,8 +3,29 @@ package com.example.demo.model;
 import java.util.List;
 
 public class RecursionStrategy implements RouteBuilderStrategy {
+
+    private Route recursion(int weightLeft, List<Transfer> availableTransfers, Route routeNow, int transferIndex) {
+        if(transferIndex == availableTransfers.size())
+            return (Route) routeNow.clone();
+
+        Route routeWithoutThisTransfer = recursion(weightLeft, availableTransfers, routeNow, transferIndex + 1);
+        Route routeWithThisTransfer = null;
+        int w = availableTransfers.get(transferIndex).getWeight();
+
+        if(w <= weightLeft){
+            routeNow.add(availableTransfers.get(transferIndex));
+            routeWithThisTransfer = recursion(weightLeft - w, availableTransfers, routeNow, transferIndex + 1);
+            routeNow.removeLast();
+        } else
+            return routeWithoutThisTransfer;
+
+        if(routeWithThisTransfer.getCost() <= routeWithoutThisTransfer.getCost())
+            return routeWithoutThisTransfer;
+
+        return routeWithThisTransfer;
+    }
     @Override
     public Route buildRoute(int maxWeight, List<Transfer> availableTransfers) {
-        return null;
+        return recursion(maxWeight, availableTransfers, new Route(), 0);
     }
 }
---
+++ src/test/java/com/example/demo/RouteBuilderStrategyTest.java
@@ -0,0 +1,70 @@
+package com.example.demo;
+
+import com.example.demo.model.DPStrategy;
+import com.example.demo.model.RecursionStrategy;
+import com.example.demo.model.RouteBuilderStrategy;
+import com.example.demo.model.Transfer;
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RouteBuilderStrategyTest extends TestCase {
+    private RouteBuilderStrategy dpRouteBuilder;
+    private RouteBuilderStrategy recursionRouteBuilder;
+
+    public void setUp() {
+        dpRouteBuilder = new DPStrategy();
+        recursionRouteBuilder = new RecursionStrategy();
+    }
+
+    public void testSimple() {
+        ArrayList<Transfer> transfers = new ArrayList<>();
+        assertTrue(dpRouteBuilder.buildRoute(0, transfers).isEmpty());
+        assertTrue(recursionRouteBuilder.buildRoute(0, transfers).isEmpty());
+    }
+
+    public void testShouldBeEmpty() {
+        ArrayList<Transfer> transfers = new ArrayList<>();
+        transfers.add(new Transfer(10, 1));
+        transfers.add(new Transfer(10, 1));
+        assertTrue(dpRouteBuilder.buildRoute(9, transfers).isEmpty());
+        assertTrue(recursionRouteBuilder.buildRoute(9, transfers).isEmpty());
+    }
+
+    public void testOneLengthRoute() {
+        ArrayList<Transfer> transfers = new ArrayList<>();
+        transfers.add(new Transfer(1, 1));
+        assert dpRouteBuilder.buildRoute(1, transfers).size() == 1;
+        assert recursionRouteBuilder.buildRoute(1, transfers).size() == 1;
+    }
+
+    public void testShouldTakeAll() {
+        ArrayList<Transfer> transfers = new ArrayList<>();
+        for(int i = 0; i < 10; i++) {
+            transfers.add(new Transfer(1, 10));
+        }
+        assert dpRouteBuilder.buildRoute(10, transfers).getCost() == 100;
+        assert recursionRouteBuilder.buildRoute(10, transfers).getCost() == 100;
+    }
+
+    public void testGivenExample() {
+        ArrayList<Transfer> transfers = new ArrayList<>();
+        transfers.add(new Transfer(5, 10));
+        transfers.add(new Transfer(10, 20));
+        transfers.add(new Transfer(3, 5));
+        transfers.add(new Transfer(8, 15));
+        assert dpRouteBuilder.buildRoute(15, transfers).getCost() == 30;
+        assert recursionRouteBuilder.buildRoute(15, transfers).getCost() == 30;
+    }
+
+    public void testSameAnswerReturned() {
+        Random rand = new Random();
+        ArrayList<Transfer> transfers = new ArrayList<>();
+        for(int i = 0; i < 20; i++) {
+            transfers.add(new Transfer(rand.nextInt(10000), rand.nextInt(100000)));
+        }
+        int maxWeight = rand.nextInt(200000);
+        assert dpRouteBuilder.buildRoute(maxWeight, transfers).getCost() == recursionRouteBuilder.buildRoute(maxWeight, transfers).getCost();
+    }
+}
